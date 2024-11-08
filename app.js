0fb30f2a58e488e95530bbef97db2f986f0f7aaa
Anggia Intan <intananggia23@gmail.com>
2024-11-08 17:36:15 +0700

    dbcrud_203

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+
 const express = require('express');
 const app = express();
-const todoRoutes = require('./routes/todo.js');
-const port = 3000;
-app.use(express.json());
-app.use('/todos', todoRoutes);
+const todoRoutes = require('./routes/tododb.js')
+const port = process.env.PORT;
 
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
